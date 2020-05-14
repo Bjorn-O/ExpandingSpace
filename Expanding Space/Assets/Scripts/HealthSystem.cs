b100f10cec08a1f6ee06872e2641a83baa324fd5
JiKon <canyounotspammethanks@gmail.com>
2020-05-14 12:48:55 +0200

    Dynamic Health System

---
+++ Expanding Space/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.ExceptionServices;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField] GameObject heartPrefab;
+    [SerializeField] GameObject brokenHeartPrefab; 
+
+    public void DrawHearts (int hearts, int maxHearts )
+    {
+        foreach(Transform child in transform)
+        {
+            Destroy(child.gameObject); 
+        }
+
+        for (int i = 0; i < maxHearts; i++)
+        {
+            if (i + 1 <= hearts)
+            {
+                GameObject heart = Instantiate(heartPrefab, transform.position, Quaternion.identity);
+                heart.transform.parent = transform; 
+            } 
+            else
+            {
+                GameObject heart = Instantiate(brokenHeartPrefab, transform.position, Quaternion.identity);
+                heart.transform.parent = transform;
+            }
+        }
+    }
+}
---
+++ Expanding Space/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int hearts = 3;
+    public int maxHearts = 3;
+    [SerializeField] HealthSystem hs;
+
+    private void Start()
+    {
+        hs.DrawHearts(hearts, maxHearts); 
+    }
+
+    public void DamagePlayer (int dmg)
+    {
+        if (hearts > 0)
+        {
+            hearts -= dmg;
+            hs.DrawHearts(hearts, maxHearts);
+        }
+
+    }
+
+    public void HealPlayer(int dmg)
+    {
+        if (hearts < maxHearts)
+        {
+            hearts += dmg;
+            hs.DrawHearts(hearts, maxHearts);
+        }
+        
+    }
+}
